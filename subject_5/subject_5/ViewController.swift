422fbae0dc3f5bc4af5f2bd2a48578419f1e4c9a
akio0911 <akio0911@gmail.com>
2021-05-11 18:01:20 +0900

    feedback

--- subject_5/subject_5/ViewController.swift
+++ subject_5/subject_5/ViewController.swift
@@ -8,31 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet private weak var inputedNumberTextField1: UITextField!
     @IBOutlet private weak var inputedNumberTextField2: UITextField!
     @IBOutlet private weak var divisionLabel: UILabel!
     @IBOutlet private weak var resultNumberLabel: UILabel!
+
     @IBAction private func calculationButton(_ sender: UIButton) {
-        
         guard let number1 = Double(inputedNumberTextField1.text!) else {
-            return alert(title: "課題5", message: "割られる数を入力してください")
+            presentAlert(title: "課題5", message: "割られる数を入力してください")
+            return
         }
+
         guard let number2 = Double(inputedNumberTextField2.text!) else {
-            return alert(title: "課題5", message: "割る数を入力してください")
+            presentAlert(title: "課題5", message: "割る数を入力してください")
+            return
         }
-        if number2 == 0 {
-            alert(title: "課題5", message: "割る数に0を入力しないでください")
+
+        guard number2 != 0 else {
+            presentAlert(title: "課題5", message: "割る数に0を入力しないでください")
+            return
         }
-        
+
         let resultNumber = number1 / number2
         resultNumberLabel.text = String(resultNumber)
     }
 
-    private func alert(title: String, message: String) {
+    private func presentAlert(title: String, message: String) {
         let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alertController, animated: true)
     }
 }
-
